592657133ba6d227445709bfc41164b6b7818252
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-04-28 23:24:02 +0700

    tambah code untuk datetime

--- lib/databarangpage.dart
+++ lib/databarangpage.dart
@@ -1,2 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+class DataBarangPage extends StatefulWidget {
+  const DataBarangPage({Key? key}) : super(key: key);
+
+  @override
+  _DataBarangPageState createState() => _DataBarangPageState();
+}
+
+class _DataBarangPageState extends State<DataBarangPage> {
+  final _formKey = GlobalKey<FormState>();
+  DateTime? _selectedDate;
+  String? _jenisTransaksi;
+  String? _jenisBarang;
+  final TextEditingController _jumlahController = TextEditingController();
+  final TextEditingController _hargaController = TextEditingController();
+
+Future<void> _selectDate(BuildContext context) async {
+  final DateTime? picked = await showDatePicker(
+    context: context,
+    initialDate: DateTime.now(),
+    firstDate: DateTime(2020),
+    lastDate: DateTime(2101),
+  );
+  if (picked != null && picked != _selectedDate) {
+    setState(() {
+      _selectedDate = picked;
+    });
+  }
+}
