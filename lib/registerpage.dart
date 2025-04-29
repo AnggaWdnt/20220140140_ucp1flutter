b21f1f5b818c670a1c2022508e0625dae97803a6
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-04-29 12:19:01 +0700

    perbaikan validasi konfimasi password jika beda

--- lib/registerpage.dart
+++ lib/registerpage.dart
@@ -15,139 +15,166 @@ class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController konfirmasiPasswordController = TextEditingController();
 
-  @override
-  void initState() {
-    super.initState();
-  }
+  bool _obscurePassword = true;
+  bool _obscureConfirmPassword = true;
+
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    final _formKey = GlobalKey<FormState>();
     return Scaffold(
       body: Form(
         key: _formKey,
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child:               Column(
+          child: Column(
+            children: [
+              SizedBox(height: 24),
+              Icon(Icons.arrow_drop_up, size: 100, color: Colors.black),
+              SizedBox(height: 16),
+              Text(
+                'DAFTAR AKUN BARU',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 32),
+
+              TextFormField(
+                controller: namaLengkapController,
+                decoration: InputDecoration(
+                  labelText: 'Nama Lengkap',
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+                ),
+                validator: (value) => value!.isEmpty ? 'Masukkan Nama Lengkap' : null,
+              ),
+              SizedBox(height: 16),
+
+              Row(
                 children: [
-                  SizedBox(height: 24),
-                  Icon(Icons.arrow_drop_up, size: 100, color: Colors.black),
-                  SizedBox(height: 16),
-                  Text(
-                    'DAFTAR AKUN BARU',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 32),
-
-                               TextFormField(
-                    controller: namaLengkapController,
-                    decoration: InputDecoration(
-                      labelText: 'Nama Lengkap',
-                      prefixIcon: Icon(Icons.person),
-                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+                  Expanded(
+                    child: TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+                      ),
+                      validator: (value) => value!.isEmpty ? 'Masukkan Email' : null,
                     ),
-                    validator: (value) => value!.isEmpty ? 'Masukan Nama Lengkap' : null,
                   ),
-                  SizedBox(height: 16),
-                               Row(
-                    children: [
-                      Expanded(
-                        child: TextFormField(
-                          controller: emailController,
-                          decoration: InputDecoration(
-                            labelText: 'Email',
-                            prefixIcon: Icon(Icons.email),
-                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-                          ),
-                          validator: (value) => value!.isEmpty ? 'Masukkan email' : null,
-                        ),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: TextFormField(
+                      controller: noHpController,
+                      decoration: InputDecoration(
+                        labelText: 'No HP',
+                        prefixIcon: Icon(Icons.phone),
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
                       ),
-                      SizedBox(width: 16),
-                      Expanded(
-                        child: TextFormField(
-                          controller: noHpController,
-                          decoration: InputDecoration(
-                            labelText: 'No HP',
-                            prefixIcon: Icon(Icons.phone),
-                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-                          ),
-                          validator: (value) => value!.isEmpty ? 'Masukkan nomor HP' : null,
-                        ),
-                      ),
-                    ],
+                      validator: (value) => value!.isEmpty ? 'Masukkan Nomor HP' : null,
+                    ),
                   ),
-                  SizedBox(height: 16),
-                                Row(
-                    children: [
-                      Expanded(
-                        child: TextFormField(
-                          controller: passwordController,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                            prefixIcon: Icon(Icons.lock),
-                            suffixIcon: Icon(Icons.visibility_off),
-                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-                          ),
-                          validator: (value) => value!.isEmpty ? 'Masukkan password' : null,
-                        ),
-                      ),
-                      SizedBox(width: 16),
-                      Expanded(
-                        child: TextFormField(
-                          controller: konfirmasiPasswordController,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: 'Konfirmasi Password',
-                            prefixIcon: Icon(Icons.lock),
-                            suffixIcon: Icon(Icons.visibility_off),
-                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-                          ),
-                          validator: (value) => value!.isEmpty ? 'Masukkan konfirmasi password' : null,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 32),
+                ],
+              ),
+              SizedBox(height: 16),
 
-                               SizedBox(
-                    width: double.infinity,
-                    height: 50,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xFFFF5722),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: passwordController,
+                      obscureText: _obscurePassword,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        prefixIcon: Icon(Icons.lock),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _obscurePassword ? Icons.visibility_off : Icons.visibility,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _obscurePassword = !_obscurePassword;
+                            });
+                          },
                         ),
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
                       ),
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(builder: (context) => const LoginPage()),
-                          );
+                      validator: (value) => value!.isEmpty ? 'Masukkan Password' : null,
+                    ),
+                  ),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: TextFormField(
+                      controller: konfirmasiPasswordController,
+                      obscureText: _obscureConfirmPassword,
+                      decoration: InputDecoration(
+                        labelText: 'Konfirmasi Password',
+                        prefixIcon: Icon(Icons.lock),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _obscureConfirmPassword ? Icons.visibility_off : Icons.visibility,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _obscureConfirmPassword = !_obscureConfirmPassword;
+                            });
+                          },
+                        ),
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Masukkan Konfirmasi Password';
                         }
+                        if (value != passwordController.text) {
+                          return 'Password tidak sama';
+                        }
+                        return null;
                       },
-                      child: Text(
-                        'Daftar',
-                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
                     ),
                   ),
-                  SizedBox(height: 16),
-                                TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/login');
-                    },
-                    child: Text(
-                      'Sudah memiliki akun? Silahkan Login disini!',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ),
+                ],
+              ),
+              SizedBox(height: 32),
 
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFFF5722),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => const LoginPage()),
+                      );
+                    }
+                  },
+                  child: Text(
+                    'Daftar',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/login');
+                },
+                child: Text(
+                  'Sudah memiliki akun? Silahkan Login disini!',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
